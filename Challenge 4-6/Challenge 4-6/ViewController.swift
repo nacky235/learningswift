55af7c2b1e7d60a277b63f6b6a223fa8205235ed
nacky235 <w@nacky.page>
2020-02-15 17:14:38 +0900

    aaa

---
+++ Challenge 4-6/Challenge 4-6/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Challenge 4-6
+//
+//  Created by 稲葉夏輝 on 2020/02/15.
+//  Copyright © 2020 稲葉夏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var shoppingList: [String] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForItems))
+        
+        title = "Oppai"
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func promptForItems() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] action in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String) {
+        return
+    }
+    
+
+}
+
